0a7cd067480e8f8e2b1bcb5c9d736d923cf903ad
harishsurangi <117253594+harishsurangi@users.noreply.github.com>
2022-12-06 11:03:56 +0530

    Update and rename singleton.classjava to singletonclass.java

---
+++ singletonclass.java
@@ -0,0 +1,13 @@
+package ;
+class Test{
+  
+ public static void main(String[] args) {
+   
+      private static Test t= new Test();
+      private Test(){
+       }
+      public static Test getTest(){
+          return t;
+        }
+ }
+}
